437f85f82a0b23e9a74a45aa461a5f1b65922f92
Shulamit Khayyat <skhayyat@student.touro.edu>
2026-02-18 17:43:21 -0500

    Used translate to ensure curve is right side up

--- src/main/java/khayyat/physics/GravityComponent.java
+++ src/main/java/khayyat/physics/GravityComponent.java
@@ -10,22 +10,22 @@ public class GravityComponent extends JComponent
     {
         super.paintComponent(g);
 
-        //given
         Force gravity = new Force(0, -9.8);
         Force scaledGravity = gravity.scale(0.001);
         double x = 0;
         double y = 0;
         Force f1 = new Force(37.0365, 28.9360);
 
+        g.translate(0, getHeight());
         g.drawOval((int) x, (int) y, 1, 1);
-        //when
+
         for (double i = 0; i < 5.0; i += 0.001)
         {
             f1 = f1.add(scaledGravity);
             Force scaledF1 = f1.scale(0.001);
             x += scaledF1.getX();
             y += scaledF1.getY();
-            g.drawOval((int) x, getHeight() - (int) y, 1, 1);
+            g.drawOval((int) x, (int) -y, 1, 1);
         }
     }
 }
